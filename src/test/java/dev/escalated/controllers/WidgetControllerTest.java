5b7089e84f1854072321c2b90efbe6f5bbf4f2e0
Matt Gros <3311227+mpge@users.noreply.github.com>
2026-04-08 11:34:13 -0400

    Fix test compilation: use @MockBean for Spring Boot 3.2.x
    @MockitoBean was introduced in Spring Boot 3.4. Spring Boot 3.2.5
    uses @MockBean from org.springframework.boot.test.mock.mockito.

--- src/test/java/dev/escalated/controllers/WidgetControllerTest.java
+++ src/test/java/dev/escalated/controllers/WidgetControllerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -29,11 +29,11 @@ class WidgetControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private TicketService ticketService;
-    @MockitoBean
+    @MockBean
     private KnowledgeBaseService knowledgeBaseService;
-    @MockitoBean
+    @MockBean
     private SatisfactionRatingService ratingService;
 
     @Test
